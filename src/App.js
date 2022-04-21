6c7c9780a0c13611a9e8cc60318402fd773b6476
David <david_sova@ukr.net>
2022-04-21 19:16:26 +0300

    Added Firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import React from "react";
 import Header from "./components/Header/Header";
+import firebase from "firebase";
 
 const App = (props) => {
+  console.log(firebase.database());
   return (
     <main>
       <div className="bigWrapper">
--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDskFbdqUGz1c0KArAU7zzu4kd1XVQtd-0",
+  authDomain: "project-seller-bb.firebaseapp.com",
+  databaseURL:
+    "https://project-seller-bb-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "project-seller-bb",
+  storageBucket: "project-seller-bb.appspot.com",
+  messagingSenderId: "260577557051",
+  appId: "1:260577557051:web:1c9e9a4c0493f782a93a1e",
+  measurementId: "G-VTR5RNG6D4",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
